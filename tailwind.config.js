9f9e529eb2ddf148d42ac0e855c020456b79789e
Bastian Soto Iturra <bsoto.it@gmail.com>
2023-09-13 17:01:07 -0300

    route fix, no aplicaba clases

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,25 @@
 
 export default {
   content: [
-    './pages/**/*.{html,js,jsx,astro}',
-    './components/**/*.{html,js,jsx,astro}',
+    './src/pages/**/*.{html,js,jsx,astro}',
+    './src/components/**/*.{html,js,jsx,astro}',
   ],
   theme: {
-    colors: {
-      mytheme: {
-        "primary": "#fef08a",
-        "secondary": "#0284c7",
-        "accent": "#f43f5e",
-        "neutral": "#2a323c",
-        "base-100": "#1d232a",
-        "info": "#0d9488",
-        "success": "#22c55e",
-        "warning": "#f59e0b",
-        "error": "#9f1239",
-      },
+    extend: {
+      colors: {
+        dark: {
+          "primary": "#fef08a",
+          "secondary": "#0284c7",
+          "accent": "#f43f5e",
+          "neutral": "#2a323c",
+          "base-100": "#1d232a",
+          "info": "#0d9488",
+          "success": "#22c55e",
+          "warning": "#f59e0b",
+          "error": "#9f1239",
+        },
+      }
     },
-    extend: {},
   },
   plugins: [],
 }
