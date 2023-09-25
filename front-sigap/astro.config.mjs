911d1da57da280dd239a5c5a2983e841983ca975
Bastian Soto Iturra <bsoto.it@gmail.com>
2023-09-25 10:54:24 -0300

    Cambio a monorepo

---
+++ front-sigap/astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config';
+import nodejs from '@astrojs/node';
+
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+    output: 'server',
+    adapter: nodejs({
+      mode: 'standalone',
+    }),
+    integrations: [
+      react({}),
+      tailwind({}),
+    ],
+});
