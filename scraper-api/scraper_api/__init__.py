96f618f7ffd3981773a8741b92b13fec5f5a3b86
Bastian Soto Iturra <bsoto.it@gmail.com>
2023-10-03 17:36:32 -0300

    Restart de proyecto

--- scraper-api/scraper_api/__init__.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-from scraper_api.endpoints import (
-    login,
-)
-
-app = FastAPI()
-
-app.include_router(login.router)
--- scraper-api/scraper_api/scraper/__init__.py
+++
@@ -1,4 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Firefox()
-driver.implicitly_wait(10)
