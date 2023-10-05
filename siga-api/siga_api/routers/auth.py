aa9ca3433925d84fe4fddd4ac881a7e35f69e4b0
Bastian Soto Iturra <bsoto.it@gmail.com>
2023-10-05 20:55:45 -0300

    login auth fixed

--- siga-api/siga_api/routers/auth.py
+++ siga-api/siga_api/routers/auth.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, HTTPException
+from typing import Annotated
 
-from .models import User
+from fastapi import APIRouter, Form, HTTPException
+
 from ..scraper import functions as fun, errors as err
 
 
@@ -8,9 +9,9 @@ router = APIRouter()
 
 
 @router.post('/login')
-async def log_in(user: User):
+async def log_in(username: Annotated[str, Form()], domain: Annotated[str, Form()], password: Annotated[str, Form()]):
     try:
-        fun.login(user.username, user.domain, user.password)
+        fun.login(username, domain, password)
     except err.InvalidCredentialsException as e:
         raise HTTPException(status_code=500, detail=e.args[0])
     except err.InvalidDomainException as e:
